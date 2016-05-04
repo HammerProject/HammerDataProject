c3a99e032e910d56a0232b52cb3d8e201b8147ba
Mauro Pelucchi <mauro.pelucchi@gmail.com>
2016-05-04 22:34:12 +0200

    Add examples

--- hammer-project/commad_start.sh
+++ hammer-project/commad_start.sh
@@ -1,6 +1,6 @@
 #for test env
 
-bin/yarn jar share/test/hammer-santamaria-0.0.2.jar org.hammer.santamaria.App datasource1
+bin/yarn jar share/test/hammer-santamaria-0.0.2.jar org.hammer.santamaria.App datasource
 bin/yarn jar share/test/hammer-santamaria-0.0.2.jar org.hammer.santamaria.App tweets lombardia
 
 bin/yarn jar share/test/hammer-pinta-0.0.2.jar org.hammer.pinta.App
