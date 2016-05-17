b7e02216dcb457fafcec559a11e30ea4228d8040
Mauro Pelucchi <mauro.pelucchi@gmail.com>
2016-05-17 21:34:03 +0200

    New examples

--- hammer-project/commad_start.sh
+++ hammer-project/commad_start.sh
@@ -35,14 +35,14 @@ bin/yarn jar share/hammer/hammer-santamaria-0.0.2.jar org.hammer.santamaria.App 
 bin/yarn jar share/hammer/hammer-pinta-0.0.2.jar org.hammer.pinta.App
 
 ## download all resources by query
-bin/yarn jar share/test/hammer-colombo-0.0.2.jar org.hammer.colombo.App share/hammer/query_1.json local 0.0 download true labels false 0.0
+bin/yarn jar share/test/hammer-colombo-0.0.2.jar org.hammer.colombo.App share/test/query_7.json local 0.0 download true labels false 0.0
 
 ## simulate download all resources by query
 bin/yarn jar share/test/hammer-colombo-0.0.2.jar org.hammer.colombo.App share/hammer/query_1.json local 0.0 download true labels true 0.0
 
 
 ## download selected resources by query
-bin/yarn jar share/hammer/hammer-colombo-0.0.2.jar org.hammer.colombo.App share/hammer/query_1.json local 0.3 download true keywords false 0.3
+bin/yarn jar share/test/hammer-colombo-0.0.2.jar org.hammer.colombo.App share/test/query_7.json local 0.3 download true keywords false 0.3
 
 
 
