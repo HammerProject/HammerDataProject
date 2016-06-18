e23e24f24b5b81b29961e1356cdf4a62f4357e81
Mauro Pelucchi <mauro.pelucchi@gmail.com>
2016-06-18 23:41:15 +0200

    query clone

--- hammer-isabella/src/main/java/org/hammer/isabella/cc/util/QueryGraphCloner.java
+++ hammer-isabella/src/main/java/org/hammer/isabella/cc/util/QueryGraphCloner.java
@@ -11,7 +11,6 @@ import org.hammer.isabella.query.IsabellaError;
 import org.hammer.isabella.query.Keyword;
 import org.hammer.isabella.query.QueryGraph;
 
-
 public class QueryGraphCloner {
 
 	// so that nobody can accidentally create an ObjectCloner object
@@ -23,7 +22,9 @@ public class QueryGraphCloner {
 			throws Exception {
 
 		for (String[] k : arrayList) {
-			if (!k[0].equals("select") && !k[0].equals("where") && !k[0].equals("from")) {
+			if (!k[0].equals("select") && !k[0].equals("where") && !k[0].equals("from") && !k[0].equals("label1")
+					&& !k[0].equals("value") && !k[0].equals("instance1") && !k[0].equals("instance")
+					&& !k[0].equals("label")) {
 				q = q.replaceAll(k[0], k[1]);
 			}
 		}
@@ -47,19 +48,6 @@ public class QueryGraphCloner {
 		}
 
 		return query;
-
-		/*
-		 * ObjectOutputStream oos = null; ObjectInputStream ois = null; try {
-		 * ByteArrayOutputStream bos = new ByteArrayOutputStream(); // A oos =
-		 * new ObjectOutputStream(bos); // B // serialize and pass the object
-		 * oos.writeObject(oldObj); // C oos.flush(); // D ByteArrayInputStream
-		 * bin = new ByteArrayInputStream(bos.toByteArray()); // E ois = new
-		 * ObjectInputStream(bin); // F // return the new object return
-		 * (QueryGraph) ois.readObject(); // G } catch (Exception e) {
-		 * e.printStackTrace(); System.out.println(
-		 * "Exception in ObjectCloner = " + e); throw (e); } finally {
-		 * oos.close(); ois.close(); }
-		 */
 	}
 
 }
