a712984052a9f37be93a7077e52d8cd56d922117
Mauro Pelucchi <mauro.pelucchi@gmail.com>
2016-12-03 18:36:28 +0100

    new hammer search

---
+++ hammer-search/src/org/hammer/search/App.java
@@ -0,0 +1,14 @@
+package org.hammer.search;
+
+
+/**
+ * App for Hammer Search Module
+ * 
+ * @author mauro.pelucchi@gmail.com
+ * @project Hammer Project - Search
+ *
+ */
+
+public class App {
+
+}
